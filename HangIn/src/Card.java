7ba53a8f8f805cd650cfb8fc1fd43d61ede75985
hummingbird3 <atomenes1@gmail.com>
2017-04-30 20:55:52 +0300

    Add files via upload

---
+++ HangIn/src/Card.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/**
+ * Created by USER1 on 30.04.2017.
+ */
+public class Card {
+
+    // properties
+
+    String poster;
+    Time time;
+    Area location;
+    int numHanging;
+    ArrayList<Person> hangingPeople;
+    int numComing;
+    ArrayList<Person> comingPeople;
+    boolean privat;
+    Group visibleGroups;
+
+
+    // constructors
+
+    // methods
+}
---
+++ HangIn/src/Place.java
@@ -0,0 +1,5 @@
+/**
+ * Created by USER1 on 30.04.2017.
+ */
+public class Place {
+}
