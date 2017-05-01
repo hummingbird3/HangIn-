d4a9482059763a47e27ef1be24a0780e0aeda40e
hummingbird3 <atomenes1@gmail.com>
2017-05-01 04:10:33 +0300

    Update Board.java

--- HangIn/src/Board.java
+++ HangIn/src/Board.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
+
 /**
  * Created by USER1 on 30.04.2017.
  */
 public class Board {
+
+    private String name;
+    ArrayList<Card> postedCards;
+
+    public Board(){
+        postedCards = new ArrayList<Card>();
+    }
+
+
 }
