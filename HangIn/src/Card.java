f8a77966f593efa5f1cfc437812636ca909e0ca8
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2017-05-07 23:34:10 +0300

    Update Card.java
    get & set methods are updated

--- HangIn/src/Card.java
+++ HangIn/src/Card.java
@@ -21,4 +21,94 @@ public class Card {
     // constructors
 
     // methods
+    public String getPoster()
+    {
+        return poster;
+    }
+    
+    public Time getTime()
+    {
+        return time;
+    }
+    
+    public Area getLocation()
+    {
+        return location;
+    }
+    
+    public int getNumHanging()
+    {
+        return numHanging;
+    }
+    
+    public ArrayList<Person> getHangingPeople()
+    {
+        return hangingPeople;
+    }
+    
+    public int getNumComing()
+    {
+        return numComing;
+    }
+    
+    public ArrayList<Person> getComingPeople()
+    {
+        return comingPeople;
+    }
+    
+    public boolean getPrivateAccount()
+    {
+        return PrivateAccount;
+    }
+    
+    public Group getVisibleGroups()
+    {
+        return visibleGroups;
+    }
+    
+    public void setPoster(String poster)
+    {
+        this.poster = poster;
+    }
+    
+  
+    public void setTime(Time time)
+    {
+        this.time = time;
+    }
+    
+    public void (Area location)
+    {
+        this.location = location;
+    }
+    
+    public void (int numHanging)
+    {
+        this.numHanging = numHanging;
+    }
+    
+    public void (ArrayList<Person> hangingPeople)
+    {
+        this.hangingPeople = hangingPeople;
+    }
+    
+    public void (int numComing)
+    {
+        this.numComing = numComing;
+    }
+    
+    public void (ArrayList<Person> comingPeople)
+    {
+        this.comingPeople = comingPeople;
+    }
+    
+    public void (boolean privateAccount)
+    {
+        this.privateAccount = privateAccount;
+    }
+    
+    public void (Group visibleGroups)
+    {
+        this.visibleGroups = visibleGroups;
+    }    
 }
