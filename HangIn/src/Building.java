50a4c492b0ae469a3df25acc987fd543793b47c3
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2017-05-07 23:21:36 +0300

    Update Building.java
    get & set methods are updated

--- HangIn/src/Building.java
+++ HangIn/src/Building.java
@@ -1,5 +1,72 @@
 /**
- * Created by USER1 on 30.04.2017.
+ * Created by Burak Korkmaz on 30.04.2017.
  */
+import java.util.*;
+
 public class Building {
+  
+  //properties
+  int sector;
+  int floorNo;
+  Area area;
+  ArrayList<Card> postedCard;
+  int numPosted;
+  
+  //constructor
+  public Building()
+  {
+    
+  }
+  
+  //get & set methods
+  public int getSector()
+  {
+    return sector;
+  }
+  
+  public int getFloorNo()
+  {
+    return floorNo;
+  }
+  
+  public Area getArea()
+  {
+    return area;
+  }
+  
+  public ArrayList<Card> getPostedCard()
+  {
+    return postedCard;
+  }
+  
+  public int getNumPosted()
+  {
+    return numPosted;
+  }
+  
+  public void setSector(int sector)
+  {
+    this.sector = sector;
+  }
+
+  public void setFloorNo(int floorNo)
+  {
+    this.floorNo = floorNo;
+  }
+  
+  public void setArea(Area area)
+  {
+    this.area = area;
+  }
+  
+  public void setPostedCard(ArrayList<Card> postedCard)
+  {
+    this.postedCard = postedCard;
+  }
+  
+  public void setNumPosted(int numPosted)
+  {
+    this.numPosted = numPosted;
+  }
+
 }
