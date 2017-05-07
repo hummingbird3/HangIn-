bca67e9e078f0848b54e3f724dcc65f562bd1666
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2017-05-07 23:06:19 +0300

    Update Board.java
    get & set methods are updated

--- HangIn/src/Board.java
+++ HangIn/src/Board.java
@@ -1,5 +1,46 @@
 /**
- * Created by USER1 on 30.04.2017.
+ * Created by Burak Korkmaz on 30.04.2017.
  */
+import java.util.*;
+
 public class Board {
+  
+  
+  //properties
+  String name;
+  ArrayList<Card>  postedCards;
+  
+  //constructor
+  
+  //builds a Board with any default information
+  public Board() {
+    name = null;
+    postedCards = null;
+  }
+  
+  //methods
+  public void addPostedCards(Card x)
+  {
+    postedCards.add(x);
+  }
+  
+  public String getName()
+  {
+    return name;
+  }
+  
+  public String getPostedCards()
+  {
+    return postedCards.toString();
+  }
+  
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  public void setPostedCards(ArrayList<Cards> postedCards)
+  {
+    this.postedCards = postedCards;
+  }
 }
