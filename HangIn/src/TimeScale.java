c22578a871de096ba88ce121d358b10eb8e61206
hummingbird3 <atomenes1@gmail.com>
2017-05-09 01:59:21 +0300

    Update TimeScale.java

--- HangIn/src/TimeScale.java
+++ HangIn/src/TimeScale.java
@@ -2,4 +2,24 @@
  * Created by USER1 on 30.04.2017.
  */
 public class TimeScale {
+	Moment start;
+	Moment end;
+
+	public TimeScale(Moment a, Moment b){
+		start = a;
+		end = b;
+	}
+
+	public TimeScale(Moment b){
+		start = new Now();
+		end = b;
+	}
+
+	public String toString(){
+		return start.toString() + "-" + end.toString();
+	}
+
+	public String toStringReadable(){
+		return start.toStringReadable() + " to " + end.toStringReadable();
+	}
 }
