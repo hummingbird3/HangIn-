c4caf9da8072ea0238e5bd2a1ac96b313934b9ab
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2017-05-08 11:54:49 +0300

    Update FilteredBoard.java
    FilteredBoard class is completed

--- HangIn/src/FilteredBoard.java
+++ HangIn/src/FilteredBoard.java
@@ -1,5 +1,30 @@
 /**
- * Created by USER1 on 30.04.2017.
+ * Created by Burak Korkmaz on 30.04.2017.
  */
+import java.util.*;
+
 public class FilteredBoard {
+  
+  //properties
+  String name;
+  
+  //constructor
+  public FilteredBoard(){
+    name = "";
+  }
+  
+  public FilteredBoard(String name){
+    this.name = name;
+  }
+  
+  //methods
+  public String getName()
+  {
+    return name;
+  }
+  
+  public void setName(String name)
+  {
+    this.name = name;
+  }
 }
