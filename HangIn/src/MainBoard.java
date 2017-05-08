f1c3464445749230445a02fad947f8d8f1db1a8f
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2017-05-08 18:41:21 +0300

    Update MainBoard.java

--- HangIn/src/MainBoard.java
+++ HangIn/src/MainBoard.java
@@ -1,5 +1,39 @@
 /**
- * Created by USER1 on 30.04.2017.
+ * Created by Burak Korkmaz on 30.04.2017.
  */
+import java.util.*;
+
 public class MainBoard {
+  
+  //properties 
+  boolean specTime;
+  Time time;
+  
+  //constructor
+  public MainBoard()
+  {
+    specTime = true;
+    time = t - 2400;
+  }
+  
+  //methods
+  public boolean getSpecTime()
+  {
+    return specTime;
+  }
+  
+  public Time getTime()
+  {
+    return time;
+  }
+  
+  public void setSpecTime(boolean specTime)
+  {
+    this.specTime = specTime;
+  }
+  
+  public void setTime(Time time)
+  {
+    this.time = time;
+  }
 }
