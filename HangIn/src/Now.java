f7795be898f681afb83b7014cfaba5db9f6bd226
hummingbird3 <atomenes1@gmail.com>
2017-05-09 01:59:05 +0300

    Update Now.java

--- HangIn/src/Now.java
+++ HangIn/src/Now.java
@@ -4,14 +4,11 @@ import java.util.GregorianCalendar;
 import java.util.Date;
 
 /**
- * Created by Enes Yıldırım on 30.04.2017.
+ * Created by Enes Yildirim on 30.04.2017.
  */
 public class Now extends Moment{
 
 	public Now(){
-		super(32980,43908); // NOT WORKING TEMP
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		Date date = new Date();
-
+		super();
 	}
 }
