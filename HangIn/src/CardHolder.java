f594b513ad160ed7ed3ea34094333614a933124c
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2017-05-08 11:34:31 +0300

    Update CardHolder.java
    add & get card methods are updated

--- HangIn/src/CardHolder.java
+++ HangIn/src/CardHolder.java
@@ -1,5 +1,27 @@
 /**
- * Created by USER1 on 30.04.2017.
+ * Created by Burak Korkmaz on 30.04.2017.
  */
+import java.util.*;
+
 public class CardHolder {
+  
+  //properties
+  ArrayList<Card> cards;
+  
+  //constructor
+  public CardHolder()
+  {
+    cards = null;
+  }
+  
+  //methods
+  public void addCard(Card x)
+  {
+    cards.add(x);
+  }
+  
+  public ArrayList<Card> getCards()
+  {
+    return cards;
+  }
 }
