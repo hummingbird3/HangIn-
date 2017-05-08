a23499eebe527aec2058f0aac44ac700f9ab46f4
hummingbird3 <atomenes1@gmail.com>
2017-05-08 22:42:24 +0300

    Update Moment.java

--- HangIn/src/Moment.java
+++ HangIn/src/Moment.java
@@ -1,5 +1,26 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
- * Created by USER1 on 30.04.2017.
+ * Created by Enes Yıldırım on 30.04.2017.
  */
-public class Moment {
+public class Moment extends Time {
+
+	private String rawTime;
+	private String formTime;
+
+	public Moment(int h, int m){
+		SimpleDateFormat sdf = new SimpleDateFormat("HH/mm/ss/dd/MM/yyyy");
+		if ( h < 0 || h > 24 || m < 0 || m > 59){
+			rawTime = "invalid";
+			formTime = "incalid";
+			System.out.println("INVALID TIME");
+		}
+		else{
+			Calendar cal = Calendar.getInstance();
+		}
+
+	}
 }
+
