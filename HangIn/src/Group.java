7ec65810523d16db808c9ba7c22c1a86f4508c36
burakkorkmaz97 <burakkorkmaz.bilkent@gmail.com>
2017-05-08 12:03:15 +0300

    Update Group.java
    get & set methods are completed

--- HangIn/src/Group.java
+++ HangIn/src/Group.java
@@ -1,5 +1,94 @@
 /**
- * Created by USER1 on 30.04.2017.
+ * Created by Burak Korkmaz on 30.04.2017.
  */
+import java.util.*;
+
 public class Group {
+  
+  //properties
+  String name;
+  String info;
+  Boolean privat;
+  ArrayList<Person> members;
+  ArrayList<Person> coordinators;
+  ArrayList<Card> postedCards;
+  int numPosted;
+  
+  //constructor
+  public Group(){
+    
+  }
+  
+  //methods
+  public String getName()
+  {
+    return name;
+  }
+  
+  public String getInfo()
+  {
+    return info;
+  }
+  
+  public Boolean getPrivate()
+  {
+    return privat;
+  }
+  
+  public ArrayList<Person> getMembers()
+  {
+    return members;
+  }
+  
+  public ArrayList<Person> getCoordinators()
+  {
+    return coordinators;
+  }
+  
+  public ArrayList<Card> getPostedCard()
+  {
+    return postedCards;
+  }
+  
+  public int getNumPosted()
+  {
+    return numPosted;
+  }
+  
+  
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  public void setInfo(String info)
+  {
+    this.info = info;
+  }
+  
+  public void setPrivate(Boolean privat)
+  {
+    this.privat = privat;
+  }
+  
+  public void setMembers(ArrayList<Person> members)
+  {
+    this.members = members;
+  }
+  
+  public void setCoordinators(ArrayList<Person> coordinators)
+  {
+    this.coordinators = coordinators;
+  }
+  
+  public void setPostedCard(ArrayList<Card> postedCard)
+  {
+    this.postedCards = postedCards;
+  }
+  
+  public void setNumPosted(int numPosted)
+  {
+    this.numPosted = numPosted;
+  }
+  
 }
