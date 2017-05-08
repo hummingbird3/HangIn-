4252b1ec0d0b474755baf32e94dbb13c51e82b6f
hummingbird3 <atomenes1@gmail.com>
2017-05-09 01:58:46 +0300

    Update Moment.java

--- HangIn/src/Moment.java
+++ HangIn/src/Moment.java
@@ -1,6 +1,5 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 /**
  * Created by Enes Yıldırım on 30.04.2017.
@@ -9,18 +8,55 @@ public class Moment extends Time {
 
 	private String rawTime;
 	private String formTime;
+	Calendar calendar;
 
 	public Moment(int h, int m){
-		SimpleDateFormat sdf = new SimpleDateFormat("HH/mm/ss/dd/MM/yyyy");
+		super();
+
 		if ( h < 0 || h > 24 || m < 0 || m > 59){
+			rawTime = "invalid";
+			formTime = "invalid";
+			System.out.println("INVALID TIME");
+		}
+		else {
+			calendar = Calendar.getInstance();
+			calendar.set(Calendar.HOUR, h);
+			calendar.set(Calendar.MINUTE, m);
+		}
+
+	}
+
+	public Moment(int h, int m, int d, int mo){
+		super();
+
+		if ( h < 0 || h > 24 || m < 0 || m > 59 || mo < 0 || mo > 12 || d < 0 || d > 31){
 			rawTime = "invalid";
 			formTime = "incalid";
 			System.out.println("INVALID TIME");
 		}
-		else{
-			Calendar cal = Calendar.getInstance();
+
+		else {
+			calendar = Calendar.getInstance();
+			calendar.set(Calendar.HOUR, h);
+			calendar.set(Calendar.MINUTE, m);
+			calendar.set(Calendar.DATE, d);
+			calendar.set(Calendar.MONTH, mo-1);
 		}
 
+	}
+
+	public Moment(){
+		super();
+
+		calendar = Calendar.getInstance();
+	}
+
+	public String toString(){
+		return dateFormat.format(calendar.getTime());
+	}
+
+	public String toStringReadable(){
+		return dateFormat2.format(calendar.getTime());
 	}
 }
 
