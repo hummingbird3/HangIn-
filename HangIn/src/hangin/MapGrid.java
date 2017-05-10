0ce47237321a8c73c257a126436dd55ddbf1359b
egehatirnaz <egehatirnaz@hotmail.com>
2017-05-10 10:04:42 +0300

    Add files via upload
    Update.

--- HangIn/src/hangin/MapGrid.java
+++ HangIn/src/hangin/MapGrid.java
@@ -9,43 +9,70 @@ package hangin;
  *
  * @author egehatirnaz
  */
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
 
 public class MapGrid extends JPanel {
 
     //properties
     private BufferedImage image;
-    
-    private int dimension;
-    private final int BORDER_WIDTH = 401;
-    private final int BORDER_HEIGHT = 401;
+    private final int MAINCAMPUS    = 0;
+    private final int ODEON         = 1;
+    private final int EASTCAMPUS    = 2;
+    private final int BILKENTCENTER = 3;
+    private final Dimension dimension;
+    private final int BORDER_WIDTH;
+    private final int BORDER_HEIGHT;
     private final int UNIT_WIDTH;
     private final int UNIT_HEIGHT;
     private final int ROW;
     private final int COLUMN;
+    private boolean showOnMapMode;
+    private int targetX;
+    private int targetY;
 
     //constructors
-    MapGrid(int dimension, int COLUMN, int ROW) {
+    MapGrid(int sector) {
         super();
-try {                
+try {          
+        if(sector == MAINCAMPUS){
           image = ImageIO.read(new File("res/maincampus.png"));
+        }else if(sector == ODEON){
+          image = ImageIO.read(new File("res/odeon.png"));
+        }else if(sector == EASTCAMPUS){
+          image = ImageIO.read(new File("res/eastcampus.png"));
+        }else if(sector == BILKENTCENTER){
+          image = ImageIO.read(new File("res/bilkentcenter.png"));
+        }else
+          image = ImageIO.read(new File("res/maincampus.png"));
+          
+        //ALL IMAGES ARE 450x700.
+        
        } catch (IOException ex) {
             // handle exception...
             System.out.println("couldn't found that img.");
        }
-        this.dimension = dimension;
-        this.COLUMN = dimension;
-        this.ROW = dimension;
-        UNIT_HEIGHT = (BORDER_WIDTH) / dimension;
-        UNIT_WIDTH = (BORDER_HEIGHT) / dimension;
+        BORDER_HEIGHT = image.getHeight();
+        BORDER_WIDTH = image.getWidth();
+        this.dimension = new Dimension (image.getWidth(), image.getHeight());
+        this.COLUMN = (int) dimension.getWidth();
+        this.ROW = (int)dimension.getHeight();
+        UNIT_HEIGHT = 10; // dimension;
+        UNIT_WIDTH = 10; // dimension;
+        showOnMapMode = false;
+        targetX = 0;
+        targetY = 0;
 
-        setPreferredSize(new Dimension(BORDER_WIDTH, BORDER_HEIGHT));
+        setPreferredSize(new Dimension( image.getWidth() + 1, image.getHeight() + 1 ));
         repaint();
+        
     }
 
     //methods
@@ -59,13 +86,30 @@ try {
         super.paintComponent(g);
 
         g.drawImage(image, 0, 0, this); 
+        /**
         g.drawRect(0, 0, BORDER_WIDTH, BORDER_HEIGHT);
         for (int i = 0; i < COLUMN; i++) {
             for (int j = 0; j < ROW; j++) {
                 g.drawRect(i * UNIT_WIDTH, j * UNIT_HEIGHT, UNIT_WIDTH, UNIT_HEIGHT);
-                g.drawString("",(UNIT_WIDTH / 2) + (i * UNIT_WIDTH), ((UNIT_HEIGHT / 2) + (j * UNIT_HEIGHT)));
+                //g.drawString("",(UNIT_WIDTH / 2) + (i * UNIT_WIDTH), ((UNIT_HEIGHT / 2) + (j * UNIT_HEIGHT)));
             }
         }
+        */
+        
+        if(showOnMapMode){
+            g.setColor(Color.MAGENTA);
+            g.fillOval(targetX, targetY, 25, 25);
+            g.drawString("here!", targetX - 5, targetY - 5);
+            
+        }
+        
+        
+    }
+    public void showOnMap(int x, int y){
+        showOnMapMode = true;
+        targetX = x;
+        targetY = y;
+        repaint();
     }
 
     /**
@@ -121,13 +165,5 @@ try {
     public int getBorderWidth() {
         return BORDER_WIDTH;
     }
-    public static void main(String[] args){
-        MapGridPanel mapGridPanel = new MapGridPanel();
-        JFrame frame = new JFrame();
-        
-      frame.add(mapGridPanel);
-      frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-      frame.pack();
-      frame.setVisible(true);
-    }
+    
 }
--- HangIn/src/hangin/MapGridPanel.java
+++ HangIn/src/hangin/MapGridPanel.java
@@ -10,14 +10,17 @@ package hangin;
  *
  * @author egehatirnaz
  */
-import java.awt.event.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
-import java.awt.Dimension;
+import javax.swing.JFrame;
 
 public class MapGridPanel extends JPanel{
    //properties
    private MapGrid canvas;
    private MouseListener myMouseListener;
+   private int clickedRow;
+   private int clickedCol;
    
    
    
@@ -25,7 +28,9 @@ public class MapGridPanel extends JPanel{
    MapGridPanel(){
       super();
       
-      canvas = new MapGrid(40,1,1);
+      clickedRow = -1;
+      clickedCol = -1;
+      canvas = new MapGrid(3);
       myMouseListener = new MyMouseListener();
       canvas.addMouseListener(myMouseListener);
       
@@ -33,7 +38,33 @@ public class MapGridPanel extends JPanel{
    }
    
    //methods
-   
+   public static void main(String[] args){
+      
+        MapGridPanel mapGridPanel = new MapGridPanel();
+        
+      JFrame frame = new JFrame();
+      
+      
+      frame.add(mapGridPanel);
+      frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+      frame.pack();
+      frame.setVisible(true);
+      
+    }
+   /**
+    * getClickedRow method returns the last pressed row
+    * @return clickedRow
+    */
+   public int getClickedRow(){
+    return clickedRow;
+}
+   /**
+    * getClickedCol method returns the last pressed col
+    * @return clickedCol
+    */
+   public int getClickedCol(){
+    return clickedCol;
+}
    /**
     * MyMouseListener class
     * @author Ege "Aegis" Hatırnaz
@@ -53,9 +84,12 @@ public class MapGridPanel extends JPanel{
                row = (e.getY() / canvas.getUnitHeight()) + 1;
                column = (e.getX() / canvas.getUnitWidth()) +1;
                //do something
-               System.out.println("kek. (row,col) "+ row + "," + column);
+               clickedRow = row;
+               clickedCol = column;
+               System.out.println("kek. (row,col) "+ clickedRow + "," + clickedCol + "\n(x,y): " + e.getX() + "," + e.getY());
             }
          }
+         canvas.showOnMap(e.getX()- 12 , e.getY() - 12);
          canvas.repaint();
          repaint();
          
