a65df06a8c556ef98058e5472d40805a2bb39359
hummingbird3 <atomenes1@gmail.com>
2017-05-13 15:03:26 +0300

    Update Building.java

--- HangIn/src/Building.java
+++ HangIn/src/Building.java
@@ -1,5 +1,15 @@
 /**
  * Created by USER1 on 30.04.2017.
  */
-public class Building {
+public class Building extends Area {
+
+	String name;
+
+	public Building( String n){
+		name = n;
+	}
+
+	public String toString(){
+		return name;
+	}
 }
